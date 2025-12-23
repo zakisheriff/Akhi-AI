e8b610949bfc9d49c503c4304c741bd9ad6e96ca
Zaki Sheriff <zakisheriff57@gmail.com>
2025-12-23 11:34:48 +0530

    Few Animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
